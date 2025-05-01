d39fcb151a9ea5e6931e87976df325e97bbfb38d
Ayush Chaudhari <ayushchaudhari3516@gmail.com>
2025-05-01 10:44:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ export default function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-links">
-        <a href="https://bnb-verifier.vercel.app/" target="_blank" rel="noopener noreferrer">Home</a>
-        <a href="https://bnb-verifier.vercel.app/" target="_blank" rel="noopener noreferrer">Blockchain</a>
+        <a href="https://bscscan.com/" target="_blank" rel="noopener noreferrer">Home</a>
+        <a href="https://bscscan.com/txs" target="_blank" rel="noopener noreferrer">Blockchain</a>
         <a href="https://bscscan.com/tokens" target="_blank" rel="noopener noreferrer">Tokens</a>
-        <a href="https://bscscan.com/tokens" target="_blank" rel="noopener noreferrer">Validators</a>
+        <a href="https://bscscan.com/validators" target="_blank" rel="noopener noreferrer">Validators</a>
         <a href="https://bscscan.com/nft-top-contracts" target="_blank" rel="noopener noreferrer">NFTs</a>
         <a href="https://bscscan.com/charts" target="_blank" rel="noopener noreferrer">Resources</a>
         <a href="https://bscscan.com/verifyContract" target="_blank" rel="noopener noreferrer">Developers</a>
